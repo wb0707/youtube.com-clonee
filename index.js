dd7221456bf105b7f35f3c795a3ee1850121f717
SR Shihab <84579722+wb0707@users.noreply.github.com>
2021-08-12 19:43:49 +0600

    youtube clone 4

---
+++ index.js
@@ -0,0 +1,554 @@
+var Videos = [
+     {
+         thumbnail: "./member1.jpg",
+        channelImg: "./member1.jpg",
+        title:"Hello everyone this me sr shihab i want to tell",
+        threeDotIcon:":",
+        channelName:"SR Shihab",
+        views:"1.4m views .",
+        uploadTime:"4 hours ago"
+     },
+     {
+      thumbnail: "./member1.jpg",
+     channelImg: "./member1.jpg",
+     title:"Hello everyone this me sr shihab i want to tell",
+     threeDotIcon:":",
+     channelName:"SR Shihab",
+     views:"1.4m views .",
+     uploadTime:"4 hours ago"
+  },
+  {
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+  thumbnail: "./member1.jpg",
+ channelImg: "./member1.jpg",
+ title:"Hello everyone this me sr shihab i want to tell",
+ threeDotIcon:":",
+ channelName:"SR Shihab",
+ views:"1.4m views .",
+ uploadTime:"4 hours ago"
+},
+
+{
+   thumbnail: "./member1.jpg",
+  channelImg: "./member1.jpg",
+  title:"Hello everyone this me sr shihab i want to tell",
+  threeDotIcon:":",
+  channelName:"SR Shihab",
+  views:"1.4m views .",
+  uploadTime:"4 hours ago"
+}
+
+
+  
+ 
+ ];
+
+
+
+
+
+Videos.map((video, i) => {
+ console.log(video);
+ var rightSide = document.querySelector(".right-side");
+ rightSide.innerHTML += ` 
+
+             <div class="video-body" key="${i}">
+               <div class="video">
+                    <img class="thumbnail" src="${video.thumbnail}" alt="thumbnail" width="100%" height="100%"/>
+                 <div class="video-header">
+                    <img src="${video.channelImg}" alt="channel-img" class="channel-img">
+                    <div class="video-title">${video.title}</div>
+                    <span class="threeDot">${video.threeDotIcon} </span>
+                </div>
+                  <div class="chanel-name-time-flex">
+                    <p class="channel-name">${video.channelName}</p>
+                    <p class="channel-views-time">
+                      <span class="views">${video.views} </span>
+                      <span class="upload-time">${video.uploadTime}</span>
+                    </p>
+                  </div>
+               </div>
+             </div>
+
+ ` 
+
+})
